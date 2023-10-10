eb7ce1b2d0a4f37f82f9bdb224c923710d3d2586
lucasschakel <lucas.schakel@gmail.com>
2023-10-10 14:53:37 +0200

    new script

--- change_slack_picture.py
+++ change_slack_picture.py
@@ -9,20 +9,17 @@ def main():
     images = [os.path.join(image_dir, f) for f in os.listdir(image_dir) if os.path.isfile(os.path.join(image_dir, f))]
     image_path = random.choice(images)
 
-    with open(image_path, 'rb') as f:
-        image_content = f.read()
-
+    # The headers now only contain the Authorization. The content type will be determined automatically by `requests` 
+    # when using the files parameter.
     headers = {
-        "Authorization": f"Bearer {os.environ['SLACK_OAUTH_TOKEN']}",
-        "Content-Type": "application/json; charset=utf-8"
+        "Authorization": f"Bearer {os.environ['SLACK_OAUTH_TOKEN']}"
     }
 
-    payload = {
-        "image": image_content
-    }
-
-    response = requests.post('https://slack.com/api/users.setPhoto', headers=headers, data=payload)
-    print(response.text)
+    # The files dictionary is used to send the image as multipart/form-data.
+    with open(image_path, 'rb') as f:
+        files = {'image': f}
+        response = requests.post('https://slack.com/api/users.setPhoto', headers=headers, files=files)
+        print(response.text)
 
 if __name__ == "__main__":
     main()
